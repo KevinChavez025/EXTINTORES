a264925e4a4137046c68a46b4aedc67b1e4469bf
KevinChavez025 <kevinchavezgeldres@gmail.com>
2026-02-17 00:32:12 -0500

    arreglando tu webada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Nosotros } from './pages/nosotros/nosotros';
 import { Servicios } from './pages/servicios/servicios';
 import { Productos } from './pages/productos/productos';
 import { Contacto } from './pages/contacto/contacto';
+import { ProductoDetalleComponent } from './pages/producto-detalle/producto-detalle';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -13,5 +14,6 @@ export const routes: Routes = [
   { path: 'servicios', component: Servicios},
   { path: 'productos', component: Productos },
   { path: 'contacto', component: Contacto },
+  {path: 'producto-detalle/:id', component: ProductoDetalleComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
