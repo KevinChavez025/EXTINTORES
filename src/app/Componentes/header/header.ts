743f29da3dcad717a6d81369bfb18f7429b2a66c
7seb4s <fernandoramossomoza@gmail.com>
2026-02-18 19:12:10 -0500

    Agregado hamburguesa, scroll reveal y parallax

--- src/app/Componentes/header/header.ts
+++ src/app/Componentes/header/header.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  menuAbierto = false;
 
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu() {
+    this.menuAbierto = false;
+  }
 }
